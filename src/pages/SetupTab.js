c9778704284f48aab0c0367cfcf230893568c9ab
Matt Putnam <matt.s.putnam@gmail.com>
2020-03-14 17:17:31 -0400

    stub out edit pedal config

--- src/pages/SetupTab.js
+++ src/pages/SetupTab.js
@@ -11,6 +11,7 @@ import { findId } from '../utils/IdFinder'
 import MidiListener from '../components/MidiListener'
 import { FaArrowUp, FaArrowDown, FaTrash } from 'react-icons/fa'
 import KeyboardSizeSelector from '../components/KeyboardSizeSelector'
+import * as Midi from '../utils/Midi'
 
 
 const KeyboardConfig = ({ keyboard, deleteSelf, midiInterfaces, moveUp, moveDown, setData }) => {
@@ -46,23 +47,69 @@ const SynthConfig = ({ synth, midiInterfaces, setData }) => {
     </Container>
 }
 
-const EditPedalConfig = ({ pedal }) => {
-    return <Container inner title='Edit Pedal'>
-        Edit Pedal
-    </Container>
-}
+class ActionPedalConfig extends React.Component {
+    constructor(props) {
+        super(props)
 
-const SustainPedalConfig = ({ pedal }) => {
-    return <Container inner title='Sustain Pedal'>
-        Sustain Pedal
-    </Container>
+        this.state = {
+            stage: undefined,
+            stage1Signals: [],
+            stage2Signals: []
+        }
+    }
+    render() {
+        return <Container inner title='Action Pedal'>
+            {this.getDisplay()}
+        </Container>
+    }
+
+    getDisplay() {
+        const { pedal, keyboards } = this.props
+
+        if (!pedal) {
+            return 'Not set up'
+        }
+        if (keyboards.length === 0) {
+            return 'No keyboards available'
+        }
+
+        const { keyboardId, control, type } = pedal
+        const keyboard = _.find(keyboards, { id: keyboardId })
+
+        if (!keyboard) {
+            return 'Keyboard not found'
+        }
+
+        const styles = {
+            labelColumn: {
+                textAlign: 'right'
+            }
+        }
+
+        return <table>
+        <tbody>
+            {keyboards.length > 1 && <tr>
+                <td style={styles.labelColumn}>Keyboard:</td>
+                <td>{keyboard.midiInterface} on channel {keyboard.channel+1}</td>
+            </tr>}
+            <tr>
+                <td style={styles.labelColumn}>Controller:</td>
+                <td>{Midi.ccNames[control].long}</td>
+            </tr>
+            <tr>
+                <td style={styles.labelColumn}>Type:</td>
+                <td>{type}</td>
+            </tr>
+        </tbody>
+    </table>
+    }
 }
 
 
 class SetupTab extends React.Component {
     render() {
         const { data, midiInterfaces, setData } = this.props
-        const { keyboards, synthesizers, editPedal, sustainPedal } = data.setup
+        const { keyboards, synthesizers, actionPedal } = data.setup
         const moveUp = (index) => () => {
             const elem = keyboards[index]
             const prev = keyboards[index-1]
@@ -92,10 +139,7 @@ class SetupTab extends React.Component {
                 <Button onClick={() => this.addKeyboard()}>
                     Add a keyboard, or press a key to auto discover
                 </Button>
-                <Flex>
-                    <EditPedalConfig pedal={editPedal}/>
-                    <SustainPedalConfig pedal={sustainPedal}/>
-                </Flex>
+                <ActionPedalConfig pedal={actionPedal} keyboards={keyboards}/>
             </Container>
             <Container title='Synthesizers'>
                 {synthesizers.map(synth =>
