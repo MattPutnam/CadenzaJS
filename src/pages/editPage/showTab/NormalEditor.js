18b7bcb6a95d33d55bb343fbb8d2bfcab749ea66
Matt Putnam <matt.s.putnam@gmail.com>
2020-03-30 18:45:35 -0400

    monophonic option in normal patch usage type

--- src/pages/editPage/showTab/NormalEditor.js
+++ src/pages/editPage/showTab/NormalEditor.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
+import { Checkbox } from '../../../components/Components'
+import { Container, Flex } from '../../../components/Layout'
 import Transpose from '../../../components/Transpose'
 
 
 const NormalEditor = ({ patchUsage, setData }) => {
+    const setMonophonic = checked => {
+        if (checked) {
+            patchUsage.monophonic = true
+        } else {
+            delete patchUsage.monophonic
+        }
+        setData('monophonic patch assignment')
+    }
+
     return <>
         <Transpose alt object={patchUsage.attributes} setData={setData}/>
-        {/* TODO: monophonic */}
+        <Container alt>
+            <Flex pad>
+                <Checkbox label='Monophonic' checked={!!patchUsage.monophonic} onChange={setMonophonic}/>
+            </Flex>
+        </Container>
     </>
 }
 
