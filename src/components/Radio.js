aec2f22b065b4943bbac4e9d1cbb8225bc9ba14a
Matt Putnam <matt.s.putnam@gmail.com>
2020-04-05 11:28:01 -0400

    pointer cursor for radio buttons

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -23,10 +23,14 @@ export const RadioButtonGroup = ({ selected, onSelected, children }) => {
 export const RadioButton = ({ checked, groupName, onClick, children }) => {
     const id = uuid()
 
+    const style = {
+        cursor: 'pointer'
+    }
+
     return (
         <Flex pad>
-            <input type='radio' id={id} name={groupName} onChange={onClick} checked={checked}/>
-            <label htmlFor={id}>{children}</label>
+            <input type='radio' id={id} name={groupName} onChange={onClick} checked={checked} style={style}/>
+            <label htmlFor={id} style={style}>{children}</label>
         </Flex>
     )
 }
