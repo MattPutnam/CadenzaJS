ad0a9469de553fd1137f1156335a2b90acaadc6d
Matt Putnam <matt.s.putnam@gmail.com>
2020-04-15 17:59:23 -0400

    cleanup

--- src/utils/SynthUtils.js
+++ src/utils/SynthUtils.js
@@ -4,12 +4,11 @@ import * as Expansions from '../synthesizers/expansions'
 import * as GM from '../synthesizers/GM.json'
 import * as Synthesizers from '../synthesizers/synthesizers'
 
-const GMTree = GM.default
-const GM1Patches = GMTree.map((topLevel, index) => ({
+const GM1Patches = GM.default.map((topLevel, index) => ({
     name: topLevel[0],
     number: index
 }))
-const GM2Patches = _.flatMap(GMTree, (topLevel, index) => {
+const GM2Patches = _.flatMap(GM.default, (topLevel, index) => {
     return topLevel.map((patch, subIndex) => ({
         name: patch,
         number: [index, subIndex]
@@ -32,29 +31,17 @@ export const resolveSynthesizersAndPatches = synthesizers => {
         
         synthDefinition.banks.forEach(bank => {
             if (bank.special) {
-                if (bank.special === 'GM1' || bank.special === 'GM') {
-                    synthItem.banks.push({
-                        name: 'GM',
-                        patches: GM1Patches
-                    })
-                    allPatches.push.apply(allPatches, GM1Patches.map(({ name, number }) => ({
-                        synthesizer: synth.name,
-                        synthesizerId: synth.id,
-                        bank: 'GM',
-                        number, name
-                    })))
-                } else if (bank.special === 'GM2') {
-                    synthItem.banks.push({
-                        name: 'GM2',
-                        patches: GM2Patches
-                    })
-                    allPatches.push.apply(allPatches, GM2Patches.map(({ name, number }) => ({
-                        synthesizer: synth.name,
-                        synthesizerId: synth.id,
-                        bank: 'GM2',
-                        number, name
-                    })))
-                }
+                const patches = bank.special === 'GM2' ? GM2Patches : GM1Patches
+                synthItem.banks.push({
+                    name: bank.special,
+                    patches: patches
+                })
+                allPatches.push.apply(allPatches, patches.map(({ name, number }) => ({
+                    synthesizer: synth.name,
+                    synthesizerId: synth.id,
+                    bank: bank.special,
+                    number, name
+                })))
             } else {
                 synthItem.banks.push({
                     name: bank.name,
